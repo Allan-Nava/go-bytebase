1120a9c60349a990b2fa5f4d8fe9e5978b146fe0
Allan Nava <allannava95@gmail.com>
2023-08-23 16:56:20 +0200

    added vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitepress'
+//
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'go-bytebase',
+  description: '',
+  base: '/go-bytebase/',
+  themeConfig: {
+    outline: [2, 3],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Allan-Nava/go-bytebase' },
+    ],
+    sidebar: [
+      { text: 'Introduction', link: '/' },
+      { text: 'Getting started', link: '/getting-started' },
+    ],
+  },
+})
