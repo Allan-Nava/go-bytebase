01b3ffebcb21532a6d6b90e886b5d05ff7f62e0f
Allan Nava <allannava95@gmail.com>
2023-08-23 16:53:57 +0200

    added request, response and constants

---
+++ bytebase/response.go
@@ -0,0 +1,19 @@
+package byteBase
+
+/*
+[
+  {
+    "code": "number",
+    "content": "string",
+    "status": "string",
+    "title": "string"
+  }
+]
+*/
+
+type Response struct {
+	Code int `json:"code"`
+	Content string `json:"content"`
+	Status string `json:"status"`
+	Title string `json:"title"`
+}
